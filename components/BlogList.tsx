dcf72133c5e7f159c4a885e8ce561ee259ed4cd9
newton mathias <newtymathias@gmail.com>
2022-12-31 20:25:19 +0300

    sanity

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -19,7 +19,7 @@ function BlogList({ posts }: Props) {
                 alt={post.author.name}
                 fill
               />
-              <div>
+              <div className="absolute bottom-0 w-full bg-opacity-20 bg-black backdrop-blur-lg rounded drop-shadow-lg text-white p-5 flex justify-between">
                 <div>
                   <p className="font-bold">{post.title}</p>
                   <p>
@@ -30,7 +30,20 @@ function BlogList({ posts }: Props) {
                     })}
                   </p>
                 </div>
+
+                <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-2 items-center">
+                  {post.categories.map((category) => (
+                    <div className="bg-[#39FF14] text-center text-black px-3 py-1 rounded-full text-sm font-semibold">
+                      <p>{category.title}</p>
+                    </div>
+                  ))}
+                </div>
               </div>
+            </div>
+
+            <div className="mt-5 flex-1">
+              <p className="underline text-lg font-bold">{post.title}</p>
+              <p className="text-gray-500">{post.description}</p>
             </div>
           </div>
         ))}
